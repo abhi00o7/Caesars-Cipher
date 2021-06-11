df0fe4389a9a685d404784c3365d51146a77c312
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-11 07:01:04 +0530

    Update rot13Decipher.js
    added test cases

--- rot13Decipher.js
+++ rot13Decipher.js
@@ -11,10 +11,27 @@ function rot13(str) {
 
 
     return str
-                .split('')
-                .map((element) => alphabet.indexOf(element) != -1 ? alphabet.indexOf(element) > 12 ? element = alphabet[alphabet.indexOf(element) - 13] : element = alphabet[alphabet.indexOf(element) + 13] : element)
-                .join('')
+        .split('')
+        .map((element) => alphabet.indexOf(element) != -1 ?
+            alphabet.indexOf(element) > 12 ?
+            element = alphabet[alphabet.indexOf(element) - 13] :
+            element = alphabet[alphabet.indexOf(element) + 13] :
+            element)
+        .join('')
 
 }
 
-console.log(rot13("SERR PBQR PNZC"))
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."))
+
+// test cases:
+
+rot13("SERR PBQR PNZC") // should decode to the string FREE CODE CAMP
+
+// Passed
+rot13("SERR CVMMN!") // should decode to the string FREE PIZZA!
+
+// Passed
+rot13("SERR YBIR?") // should decode to the string FREE LOVE?
+
+// Passed
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") // should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
